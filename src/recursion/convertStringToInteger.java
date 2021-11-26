d12cc5ed2d3779588b38b97bc36b7c8093293c39
manishdhakrey <manish.gla_cs19@gla.ac.in>
2021-11-26 20:51:03 +0530

    alternative solution

--- src/recursion/convertStringToInteger.java
+++ src/recursion/convertStringToInteger.java
@@ -7,20 +7,18 @@ public class convertStringToInteger {
     public static void main(String args[]) {
         // Your Code Here
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        System.out.println(stringToInt(s));
-
+        String str = sc.nextLine();
+        int n = str.length();
+        System.out.println(stringToInt(str, n));
+        sc.close();
     }
 
-    public static int stringToInt(String str) {
-        if (str.length() == 1)
-            return (str.charAt(0) - '0');
+    public static int stringToInt(String str, int n) {
+        if (n == 1) {
+            return str.charAt(0) - '0';
+        }
 
-        double y = stringToInt(str.substring(1));
-        double x = str.charAt(0) - '0';
-
-        x = x * Math.pow(10, str.length() - 1) + y;
-        return (int) (x);
+        return (10 * stringToInt(str, n - 1) + str.charAt(n - 1) - '0');
     }
 
 }
